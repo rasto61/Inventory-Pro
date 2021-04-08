b0bc585bcb630db578fd0ac797f7cc66bdd9b4a7
rasto61 <rasto61@gmail.com>
2021-04-08 02:32:42 +0200

    moved demos to importable unitypackage

--- Assets/Devdog/InventoryPro/Scripts/Collections/CollectionPopulator.cs
+++ Assets/Devdog/InventoryPro/Scripts/Collections/CollectionPopulator.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Devdog.General.ThirdParty.UniLinq;
-using System.Text;
-using Devdog.General;
+﻿using Devdog.General;
 using UnityEngine;
 
-namespace Devdog.InventoryPro
-{
-    [RequireComponent(typeof(ItemCollectionBase))]
-    public partial class CollectionPopulator : MonoBehaviour
-    {
+namespace Devdog.InventoryPro {
+    public partial class CollectionPopulator : MonoBehaviour {
+        public ItemCollectionBase targetCollection;
         public ItemAmountRow[] items = new ItemAmountRow[0];
 
         /// <summary>
@@ -19,36 +13,28 @@ namespace Devdog.InventoryPro
 
         private InventoryItemBase[] _items = new InventoryItemBase[0];
 
-        protected void Awake()
-        {
+        protected void Awake() {
             _items = InventoryItemUtility.RowsToItems(items, true);
-            for (int i = 0; i < _items.Length; i++)
-            {
+            for (int i = 0; i < _items.Length; i++) {
                 _items[i].transform.SetParent(transform);
                 _items[i].gameObject.SetActive(false);
             }
         }
 
-
-        protected void Start()
-        {
-            var col = GetComponent<ItemCollectionBase>();
-            if (col == null)
-            {
+        protected void Start() {
+            if (targetCollection == null)
+                targetCollection = GetComponent<ItemCollectionBase>();
+            if (targetCollection == null) {
                 DevdogLogger.LogError("CollectionPopulator can only be used on a collection.", transform);
                 return;
             }
 
-            if (useForceSet)
-            {
-                for (uint i = 0; i < _items.Length; i++)
-                {
-                    col.SetItem(i, _items[i], true);
+            if (useForceSet) {
+                for (uint i = 0; i < _items.Length; i++) {
+                    targetCollection.SetItem(i, _items[i], true);
                 }
-            }
-            else
-            {
-                col.AddItems(_items);
+            } else {
+                targetCollection.AddItems(_items);
             }
         }
     }
